88f9f11ed593d70442b1cf4cdee1d038690eba54
s-yamamoto <s-yamamoto.dev@gmail.com>
2022-10-31 08:38:24 +0900

    タイマー設定用コンポーネント作成

---
+++ src/components/MinuteSettingBtn.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="contents px-3">
+    <v-btn fab color="primary" small @click="incrementMinute">
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+    <v-text-field
+      class="centered-input"
+      outlined
+      hide-details="false"
+      v-model="minute"
+    ></v-text-field>
+    <v-btn cols="1" fab color="primary" small @click="decrementMinute">
+      <v-icon>mdi-minus</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MinuteSettingBtn",
+  props: {
+    defaultMinute: {
+      type: Number,
+      default: 25,
+    },
+  },
+  data: () => ({
+    minute: 0,
+  }),
+  created() {
+    this.minute = this.defaultMinute
+  },
+  methods: {
+    incrementMinute() {
+      this.minute = ++this.minute
+    },
+    decrementMinute() {
+      this.minute = --this.minute
+    },
+  },
+}
+</script>
+
+<style scoped>
+.contents {
+  width: 180px;
+  display: flex;
+  align-items: center;
+}
+</style>
+<style>
+.centered-input input {
+  text-align: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
-  <h1>Home</h1>
+  <v-main>
+    <h1>Home</h1>
+    <MinuteSettingBtn></MinuteSettingBtn>
+    <MinuteSettingBtn :default-minute="5"></MinuteSettingBtn>
+    <MinuteSettingBtn></MinuteSettingBtn>
+  </v-main>
 </template>
 
 <script>
+import MinuteSettingBtn from "@/components/MinuteSettingBtn"
 export default {
   name: "Home",
+  components: {
+    MinuteSettingBtn,
+  },
   data: () => ({
     //
   }),
-};
+}
 </script>
