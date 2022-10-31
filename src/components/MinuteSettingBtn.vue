21e139a81da047e069c8314bf1d784d8bd93f22e
s-yamamotoD <syamamoto.dev@gmail.com>
2022-10-31 09:11:56 +0900

    タイマー設定コンポーネントにラベルを追加

--- src/components/MinuteSettingBtn.vue
+++ src/components/MinuteSettingBtn.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="contents px-3">
-    <v-btn fab color="primary" small @click="incrementMinute">
-      <v-icon>mdi-plus</v-icon>
-    </v-btn>
-    <v-text-field
-      class="centered-input"
-      outlined
-      hide-details="false"
-      v-model="minute"
-    ></v-text-field>
-    <v-btn cols="1" fab color="primary" small @click="decrementMinute">
-      <v-icon>mdi-minus</v-icon>
-    </v-btn>
+  <div class="contents">
+    <span>{{ leftLabel }}</span>
+    <div class="items px-3">
+      <v-btn fab color="primary" small @click="incrementMinute">
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+      <v-text-field
+        class="centered-input"
+        outlined
+        hide-details="false"
+        v-model="minute"
+      ></v-text-field>
+      <v-btn cols="1" fab color="primary" small @click="decrementMinute">
+        <v-icon>mdi-minus</v-icon>
+      </v-btn>
+    </div>
+    <span>{{ rightLabel }}</span>
   </div>
 </template>
 
@@ -22,6 +26,14 @@ export default {
     defaultMinute: {
       type: Number,
       default: 25,
+    },
+    leftLabel: {
+      type: String,
+      default: "",
+    },
+    rightLabel: {
+      type: String,
+      default: "",
     },
   },
   data: () => ({
@@ -43,6 +55,10 @@ export default {
 
 <style scoped>
 .contents {
+  display: flex;
+  align-items: center;
+}
+.items {
   width: 180px;
   display: flex;
   align-items: center;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
   <v-main>
     <h1>Home</h1>
-    <MinuteSettingBtn></MinuteSettingBtn>
-    <MinuteSettingBtn :default-minute="5"></MinuteSettingBtn>
-    <MinuteSettingBtn></MinuteSettingBtn>
+    <MinuteSettingBtn
+      left-label="Mob timer"
+      right-label="minutes"
+    ></MinuteSettingBtn>
+    <MinuteSettingBtn
+      :default-minute="5"
+      left-label="Break after"
+      right-label="typists (10 minutes)"
+    ></MinuteSettingBtn>
+    <MinuteSettingBtn
+      left-label="Coffee break"
+      right-label="minutes"
+    ></MinuteSettingBtn>
   </v-main>
 </template>
 
